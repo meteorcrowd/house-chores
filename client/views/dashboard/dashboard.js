a09ce8b14d27ecc81cb1503a2366b2cf8b0fe7fd
nizyev <illya.nizyev@gmail.com>
2014-12-06 17:50:28 +0200

    autoform added

--- client/views/dashboard/dashboard.js
+++ client/views/dashboard/dashboard.js
@@ -5,23 +5,6 @@ Template.dashboard.rendered = function() {
 Template.myHomes.helpers({
     'myHomes': function() {
         return Homes.find().fetch();
-        console.log("hello!");
     }
 });
 
-Template.myHomes.events({
-    'click .remove': function(event){
-        Meteor.call('removeHomeData', this._id);
-    }
-})
-
-
-Template.addHomeForm.events({
-    'submit form': function(event){
-        event.preventDefault();
-        Homes.insert({
-            title: event.target.homeTitle.value
-        })
-    }
-    
-})
--- collections/homes.js
+++ collections/homes.js
@@ -1,1 +1,29 @@
 Homes = new Mongo.Collection('homes');
+
+var Schemas = {};
+
+Schemas.Home = new SimpleSchema({
+    title: {
+        type: String,
+        label: "Title",
+        max: 200
+    },
+    address: {
+        type: String,
+        label: "Address",
+        max: 200
+    },
+    city: {
+        type: String,
+        label: "City",
+        max: 200
+    },
+    country: {
+        type: String,
+        label: "Country",
+        max: 200
+    }
+
+});
+
+Homes.attachSchema(Schemas.Home);
